22c43f2b8c8b0cc76f328012bfcc4f124bc72b9b
sehnsucht <tece@sehnsucht.top>
2024-12-24 16:17:34 +0800

    feat: add title and tags

--- src/routes/contents/[...filename].tsx
+++ src/routes/contents/[...filename].tsx
@@ -7,7 +7,6 @@ export default function BlogPage() {
     const params = useParams();
 
     const currentPost = createMemo(() => {
-        console.log(params.filename);
         let article = null;
         const decodedFilename = decodeURIComponent(params.filename); // Decode URL parameter
 
@@ -22,7 +21,20 @@ export default function BlogPage() {
 
     return (
         <div class="container py-8">
-            <article class="prose prose-lg dark:prose-invert">
+            <h1 class="text-5xl font-bold mb-4 text-gray-900">
+                {currentPost().attributes.title}
+            </h1>
+
+            <div class="flex flex-wrap gap-2 pb-10">
+                {currentPost().attributes.tags?.map((tag: string, index: number) => (
+                    <span
+                        class="px-3 py-1 text-sm bg-gray-100 text-gray-700 rounded-full hover:bg-gray-200 transition-colors"
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+            <article class="prose prose-base dark:prose-invert">
                 <div innerHTML={currentPost()?.html} />
             </article>
         </div>
